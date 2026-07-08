07845ab4ca93addbee840261f66234edf4798092
martinyazlle <martinyazlle@gmail.com>
2026-07-08 12:33:59 -0300

    Update panaleramoldesdev.py

--- panaleramoldesdev.py
+++ panaleramoldesdev.py
@@ -9,10 +9,6 @@ import json
 import pydeck as pdk
 import uuid
 
-st.sidebar.write("Estado de conexión:")
-st.sidebar.write("URL configurada:", "SÍ" if os.getenv("SUPABASE_URL") else "NO")
-st.sidebar.write("KEY configurada:", "SÍ" if os.getenv("SUPABASE_KEY") else "NO")
-
 if st.button("Forzar recarga de datos"):
     if 'df_prod' in st.session_state:
         del st.session_state['df_prod']
